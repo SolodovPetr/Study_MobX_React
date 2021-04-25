13dc9e50052d214c7b7709eec8f4d2e71aa86098
Petr <solodov.petr@gmail.com>
2021-04-25 22:11:22 +0300

    ServerApi and config for json-server

---
+++ src/api/index.js
@@ -0,0 +1,44 @@
+import qs from "query-string";
+
+export const ENDPOINT = "//localhost:3004/";
+
+class ServerApi {
+  constructor(domain) {
+    this.domain = domain;
+  }
+
+  async perform(url, data, config) {
+    const request = await fetch(`${this.domain}${url}`, {
+      ...config,
+      body: JSON.stringify(data),
+      headers: {
+        "Content-type": "application/json",
+      },
+    });
+    return await request.json();
+  }
+
+  async get(path, searchParams) {
+    return await this.perform(`${path}?${qs.stringify(searchParams)}`);
+  }
+
+  async post(path, payload) {
+    return await this.perform(path, payload, {
+      method: "POST",
+    });
+  }
+
+  async put(path, payload) {
+    return await this.perform(path, payload, {
+      method: "PUT",
+    });
+  }
+
+  async delete(path) {
+    return await this.perform(path, null, {
+      method: "DELETE",
+    });
+  }
+}
+
+export default new ServerApi(ENDPOINT);
